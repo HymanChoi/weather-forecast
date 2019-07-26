d77f010f85a774d3a6a81bfb7b89021e76cde7d1
HymanChoi <HymanChoi@outlook.com>
2019-07-26 14:22:50 +0800

    update

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,118 +3,64 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    scrollHeight: 0
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
+
+    this.calcScrollHeight();
+
+    // 页面加载完成，获取用户位置信息
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        const latitude = res.latitude;
+        const longitude = res.longitude;
+        const key = '75e1f7a8491aa6ad52d173477cf433d1';
+        const api = 'http://apis.juhe.cn/simpleWeather/query?city=';
+        const city = '广州';
+
+        // 坐标逆解析
+        // wx.request({
+        //   url: 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + res.latitude + ',' + res.longitude + '&key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&get_poi=1',
+        //   header: {
+        //     'content-type': 'application/json'
+        //   },
+        //   success(res) {
+
+        //     console.log(res.data);
+        //   }
+        // })
+
+        // 请求天气信息
+        // wx.request({
+        //   url: api + city + '&key=' + key,
+        //   header: {
+        //     'content-type': 'application/json'
+        //   },
+        //   success(res) {
+        //     console.log(res.data);
+        //   }
+        // })
+
+      }
+    })
+  },
+
+  calcScrollHeight() {
+    let that = this;
+    let query = wx.createSelectorQuery().in(this);
+    query.select('.top').boundingClientRect(function(res) {
+      // top高度
+      let topHeight = res.height;
+      // 屏幕高度
+      let screenHeight = wx.getSystemInfoSync().windowHeight;
+      // 滑动高度
+      let scrollHeight = screenHeight - topHeight - 70;
+
+      that.setData({
+        scrollHeight: scrollHeight
       })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
     })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
-
+  }
 })
